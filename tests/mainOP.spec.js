5fedf7bdfbfeaffd190e1df284a6e4890fc2fddd
Soldgame <102967432+Soldgame@users.noreply.github.com>
2025-02-02 13:51:03 +0300

    modified:   tests/mainOP.spec.js 	new file:   tests/mainPage.js

--- tests/mainOP.spec.js
+++ tests/mainOP.spec.js
@@ -1,19 +1,52 @@
-// @ts-check
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import { faker } from "@faker-js/faker";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const URL_UI = "https://realworld.qa.guru/#/";
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// Автоизация + создание статьи.
+test("Регистарция и создание новой статьи", async ({ page }) => {
+  await page.goto("https://realworld.qa.guru/#");
+  await page.getByRole("link", { name: "Sign up" }).click();
+  await page.getByRole("textbox", { name: "Your Name" }).click();
+  await page.getByRole("textbox", { name: "Your Name" }).fill("ertyui");
+  await page.getByRole("textbox", { name: "Email" }).click();
+  await page.getByRole("textbox", { name: "Email" }).fill("ewtererte");
+  await page.getByRole("textbox", { name: "Password" }).click();
+  await page
+    .getByRole("textbox", { name: "Password" })
+    .fill("dsfdsfsdf@dsfsdf.sdfsdf");
+  await page.getByRole("button", { name: "Sign up" }).click();
+  await page.getByRole("textbox", { name: "Email" }).click();
+  await page
+    .getByRole("textbox", { name: "Email" })
+    .fill("ewtererte@sdsdfsdds.dsfdsfsdf");
+  await page.getByRole("textbox", { name: "Email" }).click();
+  await page.getByRole("button", { name: "Sign up" }).click();
+  await expect(page.getByRole("navigation")).toContainText("ertyui");
+  await expect(page.getByRole("navigation")).toContainText("New Article");
+  await page.getByRole("link", { name: "New Article" }).click();
+  await expect(page.getByRole("button")).toContainText("Publish Article");
+  await page.getByRole("textbox", { name: "Article Title" }).click();
+  await page
+    .getByRole("textbox", { name: "Article Title" })
+    .fill("sthgfhfghfghfgh");
+  await page
+    .getByRole("textbox", { name: "What's this article about?" })
+    .click();
+  await page
+    .getByRole("textbox", { name: "What's this article about?" })
+    .fill("sghfghgfhsghgsh");
+  await page.getByRole("textbox", { name: "Write your article (in" }).click();
+  await page
+    .getByRole("textbox", { name: "Write your article (in" })
+    .fill("sghsghsghsghshf");
+  await page.getByRole("button", { name: "Publish Article" }).click();
+  await expect(
+    page.getByRole("heading", { name: "sthgfhfghfghfgh" })
+  ).toBeVisible();
+  await expect(page.getByRole("heading")).toContainText("sthgfhfghfghfgh");
+  await expect(page.getByRole("paragraph")).toContainText("sghsghsghsghshf");
+  await expect(
+    page.getByRole("button", { name: "Post Comment" })
+  ).toBeVisible();
 });
---
+++ tests/mainPage.js
@@ -0,0 +1,10 @@
+//Объекты
+
+class MainPage {
+  // props
+  basic_url = "https://realworld.qa.guru/#";
+  constructor(basic_url) {
+    this.basic_url = basic_url;
+  }
+  //метод объекта
+}
