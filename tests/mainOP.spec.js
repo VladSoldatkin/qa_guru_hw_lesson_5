45302e8ae603510e5fc7481580678541428969ba
Soldgame <102967432+Soldgame@users.noreply.github.com>
2025-02-03 23:39:07 +0300

    modified:   tests/mainOP.spec.js 	deleted:    tests/mainPage.js

--- tests/mainOP.spec.js
+++ tests/mainOP.spec.js
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { faker } from "@faker-js/faker";
-
+import { MainPage } from "../src/pages/mainPage";
+import { RegisterPage } from "../src/pages/registerPage";
 const URL_UI = "https://realworld.qa.guru/#";
 
 //test suit
@@ -11,49 +12,23 @@ test.describe("3 test", () => {
   });
   // Автоизация + создание статьи.
   test("Регистарция и создание новой статьи", async ({ page }) => {
-    await page.goto("https://realworld.qa.guru/#");
-    await page.getByRole("link", { name: "Sign up" }).click();
-    await page.getByRole("textbox", { name: "Your Name" }).click();
-    await page.getByRole("textbox", { name: "Your Name" }).fill("ertyui");
-    await page.getByRole("textbox", { name: "Email" }).click();
-    await page.getByRole("textbox", { name: "Email" }).fill("ewtererte");
-    await page.getByRole("textbox", { name: "Password" }).click();
-    await page
-      .getByRole("textbox", { name: "Password" })
-      .fill("dsfdsfsdf@dsfsdf.sdfsdf");
-    await page.getByRole("button", { name: "Sign up" }).click();
-    await page.getByRole("textbox", { name: "Email" }).click();
-    await page
-      .getByRole("textbox", { name: "Email" })
-      .fill("ewtererte@sdsdfsdds.dsfdsfsdf");
-    await page.getByRole("textbox", { name: "Email" }).click();
-    await page.getByRole("button", { name: "Sign up" }).click();
-    await expect(page.getByRole("navigation")).toContainText("ertyui");
+    // данные пользователя;
+    const USER = {
+      username: faker.person.firstName(),
+      email: faker.internet.email(),
+      password: faker.internet.password({ length: 10 }),
+    };
+    // объекты
+    const mainPage = new MainPage(page);
+    const registerPage = new RegisterPage(page);
+
+    //действия с основной страницей (переход на форму Регистарции);
+    await mainPage.open(URL_UI);
+    await mainPage.gotoRegister();
+
+    //действяи с формой регистрации (создание пользователя);
+    await registerPage.register(USER.username, USER.email, USER.password);
+    await expect(page.getByRole("navigation")).toContainText(USER.username);
     await expect(page.getByRole("navigation")).toContainText("New Article");
-    await page.getByRole("link", { name: "New Article" }).click();
-    await expect(page.getByRole("button")).toContainText("Publish Article");
-    await page.getByRole("textbox", { name: "Article Title" }).click();
-    await page
-      .getByRole("textbox", { name: "Article Title" })
-      .fill("sthgfhfghfghfgh");
-    await page
-      .getByRole("textbox", { name: "What's this article about?" })
-      .click();
-    await page
-      .getByRole("textbox", { name: "What's this article about?" })
-      .fill("sghfghgfhsghgsh");
-    await page.getByRole("textbox", { name: "Write your article (in" }).click();
-    await page
-      .getByRole("textbox", { name: "Write your article (in" })
-      .fill("sghsghsghsghshf");
-    await page.getByRole("button", { name: "Publish Article" }).click();
-    await expect(
-      page.getByRole("heading", { name: "sthgfhfghfghfgh" })
-    ).toBeVisible();
-    await expect(page.getByRole("heading")).toContainText("sthgfhfghfghfgh");
-    await expect(page.getByRole("paragraph")).toContainText("sghsghsghsghshf");
-    await expect(
-      page.getByRole("button", { name: "Post Comment" })
-    ).toBeVisible();
   });
 });
